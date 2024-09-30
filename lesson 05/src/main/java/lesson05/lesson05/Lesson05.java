6b37529ecc3f3cc2b0873826f362930efe4c699d
ilia543 <mrmainxpx@gmail.com>
2024-09-30 12:36:35 +0400

    lesson

---
+++ lesson 05/src/main/java/lesson05/lesson05/Lesson05.java
@@ -0,0 +1,17 @@
+package lesson05.lesson05;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lesson05 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
